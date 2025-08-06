c5a46a76f5d575ed3aae2d838544c6e1542005de
yasin_wolf <yasinkhoshabkht1400@gmail.com>
2025-08-06 22:48:28 +0330

    fourth version

---
+++ frontend/start/src/app/(auth)/signup/page.jsx
@@ -0,0 +1,81 @@
+"use client";
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import SpinnerMini from "@/ui/SpinnerMini";
+
+const schema = yup
+  .object({
+    name: yup
+      .string()
+      .min(5, "نام و نام خانوادگی نامعتبر است")
+      .max(30)
+      .required("نام و نام خانوادگی الزامی است"),
+    email: yup.string().email("ایمیل نامعتبر است").required("ایمیل الزامی است"),
+    password: yup.string().required("رمز عبور الزامی است"),
+  })
+  .required();
+
+function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = async (values) => {
+    console.log("Form Data:", values);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-12 px-6 py-8 bg-white shadow-md rounded-2xl">
+      <h1 className="text-2xl font-bold text-secondary-600 text-center mb-8">
+        ثبت‌ نام
+      </h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <RHFTextField
+          label="نام و نام خانوادگی"
+          name="name"
+          register={register}
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label="ایمیل"
+          name="email"
+          register={register}
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label="رمز عبور"
+          name="password"
+          register={register}
+          type="password"
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+
+        <div>
+          {isSubmitting ? (
+            <SpinnerMini />
+          ) : (
+            <Button type="submit" variant="primary" className="w-full">
+              تایید
+            </Button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
