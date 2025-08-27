dbecf701a9043a8fd9be4b178c4153b5ecf830f4
yasin_wolf <yasinkhoshabkht1400@gmail.com>
2025-08-28 00:11:39 +0330

    sixth version

---
+++ frontend/start/src/app/(auth)/signin/page.jsx
@@ -0,0 +1,85 @@
+"use client";
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import SpinnerMini from "@/ui/SpinnerMini";
+import { singinApi } from "@/services/authService";
+import toast from "react-hot-toast";
+
+const schema = yup
+  .object({
+    email: yup.string().email("ایمیل نامعتبر است").required("ایمیل الزامی است"),
+    password: yup.string().required("رمز عبور الزامی است"),
+  })
+  .required();
+
+function Signin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = async (values) => {
+    try {
+      const data = await singinApi(values);
+      toast.success("ورود با موفقیت انجام شد!");
+      console.log("DATA:", data);
+    } catch (err) {
+      toast.error(err?.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-12 px-6 py-8 bg-white shadow-md rounded-2xl">
+      <h1 className="text-2xl font-bold text-secondary-600 text-center mb-8">
+        ورود
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <RHFTextField
+          label="ایمیل"
+          name="email"
+          register={register}
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label="رمز عبور"
+          name="password"
+          register={register}
+          type="password"
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <div>
+          {isSubmitting ? (
+            <SpinnerMini />
+          ) : (
+            <Button type="submit" variant="primary" className="w-full">
+              تایید
+            </Button>
+          )}
+        </div>
+      </form>
+      <p className="mt-6 text-sm flex justify-center gap-1">
+        حساب نداری؟{" "}
+        <Link
+          href="/signup"
+          className="text-secondary-500 font-semibold hover:text-blue-600"
+        >
+          ثبت نام
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+export default Signin;
--- frontend/start/src/components/navbar.jsx
+++ frontend/start/src/components/navbar.jsx
@@ -1,27 +1,44 @@
 "use client";
 import React from "react";
 import NavLink from "./NavLink";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <nav className="bg-indigo-100 p-4 rounded-md shadow-md mb-6">
-      <ul className="flex flex-wrap gap-6 justify-center">
-        <li>
-          <NavLink path="/">خانه</NavLink>
-        </li>
-        <li>
-          <NavLink path="/blogs">بلاگ</NavLink>
-        </li>
-        <li>
-          <NavLink path="/dashboard">داشبورد</NavLink>
-        </li>
-        <li>
-          <NavLink path="/about">درباره ما</NavLink>
-        </li>
-        <li>
-          <NavLink path="/contact">تماس با ما</NavLink>
-        </li>
-      </ul>
+      <div className="flex items-center justify-between max-w-5xl mx-auto">
+        <ul className="flex flex-wrap gap-6 justify-center flex-grow">
+          <li>
+            <NavLink path="/">خانه</NavLink>
+          </li>
+          <li>
+            <NavLink path="/blogs">بلاگ</NavLink>
+          </li>
+          <li>
+            <NavLink path="/dashboard">داشبورد</NavLink>
+          </li>
+          <li>
+            <NavLink path="/about">درباره ما</NavLink>
+          </li>
+          <li>
+            <NavLink path="/contact">تماس با ما</NavLink>
+          </li>
+        </ul>
+        <div className="flex items-center gap-3">
+          <Link
+            href="/signin"
+            className="px-4 py-2 rounded-xl border border-indigo-500 text-indigo-600 hover:bg-indigo-200 transition"
+          >
+            ورود
+          </Link>
+          <Link
+            href="/signup"
+            className="px-4 py-2 rounded-xl bg-indigo-600 text-white hover:bg-indigo-700 transition shadow"
+          >
+            ثبت‌ نام
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
